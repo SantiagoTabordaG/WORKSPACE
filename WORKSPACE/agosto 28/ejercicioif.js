7f91fe4164d1e64e87f1e49ed981ced55cb5d5b5
SantiagoTabordaG <santiagotabordag@ielacandelariamedellin.edu.co>
2025-08-28 16:34:53 -0500

    Add files via upload

---
+++ WORKSPACE/agosto 28/ejercicioif.js
@@ -0,0 +1,21 @@
+function positivo() {
+    const numero = document.getElementById("numero");
+    const numeroInt = parseInt(numero.value);
+
+    if(numeroInt>=0) {
+        alert("El numero es positivo");
+    }else {
+        alert("El numero es negativo");
+    }
+}
+
+function esPar(){
+    const numero = document.getElementById("par");
+    const numeroInt = parseInt(numero.value);
+
+    if(numeroInt %2 == 0) {
+        alert ("El numero es par");
+    } else{
+        alert ("El numero es impar");
+    }
+}
